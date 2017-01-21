3ce5c0b324d2676832f4591ee78509bc1a9ca395
mongoos2006 <michael.d.damato@gmail.com>
2017-01-21 15:46:01 -0500

    working

--- pi-code-2/recompile.sh
+++ pi-code-2/recompile.sh
@@ -1,2 +1,3 @@
+chmod +x gradlew
 ./gradlew clean jar copyDeps
  java -jar build/libs/pi-code-2-1.0.jar -clientEndpoint awd6cf2pb49fo.iot.us-east-1.amazonaws.com -clientId sdk-java -certificateFile Raspberry-Pi.cert.pem -privateKeyFile Raspberry-Pi.private.key -thingName Raspberry-Pi
