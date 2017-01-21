2263864e17585304e5fe031bbc0db588e22daffc
mongoos2006 <michael.d.damato@gmail.com>
2017-01-21 13:01:43 -0500

    grdle

--- pi-code/recompile.sh
+++
@@ -1,3 +0,0 @@
-cd aws-iot-device-sdk-java
-mvn clean install -Dgpg.skip=true -Djava.net.preferIPv4Stack=true
-mvn exec:java -pl aws-iot-device-sdk-java-samples -Dexec.mainClass="com.amazonaws.services.iot.client.sample.shadow.ShadowSample" -Dexec.args="-clientEndpoint awd6cf2pb49fo.iot.us-east-1.amazonaws.com -clientId sdk-java -certificateFile ../Raspberry-Pi.cert.pem -privateKeyFile ../Raspberry-Pi.private.key -thingName Raspberry-Pi" -Djava.net.preferIPv4Stack=true
